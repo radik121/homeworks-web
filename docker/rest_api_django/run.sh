ef5773b0a156131b202017d3256dcd86420b90b4
Radik Masagutov <radik121@mail.ru>
2022-05-25 11:08:47 +0300

    Add docker hw

---
+++ docker/rest_api_django/run.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Collect static files
+# echo "Collect static files"
+# python manage.py collectstatic --noinput
+
+# Apply database migrations
+echo "Apply database migrations"
+python /app/manage.py migrate --noinput
+
+# Start server
+echo "Starting server"
+python /app/manage.py runserver 0.0.0.0:8000
